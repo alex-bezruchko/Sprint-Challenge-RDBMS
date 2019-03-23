72e89e5b636c44ae356c3e7053f033526a5926bd
Alex Bezruchko <alex.bezruchko@gmail.com>
2019-03-22 21:36:07 -0400

    fix text description in actions

--- data/migrations/20190322213110_actions.js
+++ data/migrations/20190322213110_actions.js
@@ -3,10 +3,10 @@ exports.up = function(knex, Promise) {
   return knex.schema
     .createTable('actions', table => {
         table.increments()
-        table.text('name').notNullable().unique()
+        table.text('action_name').notNullable().unique()
     })
 };
 
 exports.down = function(knex, Promise) {
-    return knex.schema.dropTableIfExists('ingredients')
+    return knex.schema.dropTableIfExists('actions')
 };
