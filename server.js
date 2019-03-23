da4d8db2995ee5acb77639492c4fef689ef9688b
Alex Bezruchko <alex.bezruchko@gmail.com>
2019-03-22 22:03:57 -0400

    add actionsDb helper

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 const express = require('express');
-
 const server = express();
+const actionsDb = require('./data/helpers/actionsDb');
+
 server.use(express.json());
 
+server.get('/api/actions/', async (req, res) => {
+    try {
+        const actions = await actionsDb.getAllActions()
+        if (actions) {
+            res.status(200).json(actions)
+        }
+        else {
+            res.status(404).json('No actions available')
+        }
+    }
+    catch (e) {
+        res.status(500).json(e)
+    }
+});
 
 
 module.exports = server;
