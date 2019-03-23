5d4a19969e24d507be7b29ea92b027fbc4329fbd
Alex Bezruchko <alex.bezruchko@gmail.com>
2019-03-22 21:55:34 -0400

    additional fields to projects migrations

--- data/migrations/20190322214110_projects.js
+++ data/migrations/20190322214110_projects.js
@@ -2,6 +2,9 @@
 exports.up = function(knex, Promise) {
     return knex.schema.createTable('projects', table => {
         table.increments();
+        table.text('project_name')
+        table.text('project_description')
+        table.boolean('completed')
         table
             .integer('actionProjectsId ')
             .references('id')
